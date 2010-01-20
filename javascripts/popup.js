f737dcd92d42e7ce593afd2d55080299bf57f804
Jan Andersson <jan.andersson@gear.se>
2010-01-20 11:46:05 +0100

    Show at most 10 notes and at most 70 characters of text

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -3,11 +3,12 @@ $(document).ready(function() {
     if (success) {
       chrome.extension.sendRequest({action: "index"}, function(data) {
         $('#loader').hide();
-        for(var i=0; i < data.length; i++) {
+        for(var i=0; i < (data.length > 10 ? 10 : data.length); i++) {
           if (!data[i].deleted) {
             $('#notes').append("<li id='" + data[i].key + "'></li>");
             chrome.extension.sendRequest({action: "note", key: data[i].key}, function(data) {
-              $('#' + data.key).text(data.text);
+              var lines = data.text.split("\n", 10).filter(function(line) { return ( line.length > 0 ) });
+              $('#' + data.key).html(lines[0] + "<div class='abstract'>" + lines.slice(1,3).map(function(element) { var short = element.substr(0, 67); return (short.length + 3 < element.length ? short + "..." : element ) }).join("<br />") + "</div>");
             });
           }
         }

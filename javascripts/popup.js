0fb633d98963d98591dfaf219f2382c0bf49ed79
Jan Andersson <jan.andersson@travelpartner.se>
2010-02-10 14:10:06 +0100

    Add link to Simplenote

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   if (!localStorage.email || !localStorage.password) {
     $('#loader').hide();
     $('#toolbar').hide();
-    $('#status').html("Please enter your Simplenote credentials in Options!");
+    $('#status').html("Please enter your <a target='_blank' href='http://simplenoteapp.com/home2/'>Simplenote</a> credentials in Options!");
   } else {
     chrome.extension.sendRequest({action: "login"}, function(success) {
       if (success) {
